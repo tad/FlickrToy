06dd9bf9beedf34f76a2f29e98025bef06b79d28
Tad Donaghe <terry.donaghe@gmail.com>
2012-10-23 15:19:32 -0700

    Removing Azure stuff

--- TadsFlickrToy.Azure/csx/Release/roles/TadsFlickrToy/approot/Scripts/behavior.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function () {
-    $('.refresh').click(function () {
-        location.reload();
-    });
-    $('.scroll-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, "fast");
-    });
-});
